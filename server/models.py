a0fca08c072965afc9571e55c88c90e95a2a262b
Stephen Ochieng <materiamystic@gmail.com>
2024-07-15 01:45:05 +0300

    refactors models

--- server/models.py
+++ server/models.py
@@ -4,37 +4,5 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here! Mock models
-class Doctor(db.Model, SerializerMixin):
-    __tablename__ = 'doctors'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    specialty = db.Column(db.String(100), nullable=False)
-    
-    appointments = db.relationship('Appointment', back_populates='doctor', cascade='all, delete-orphan')
-    
 
-class Appointment(db.Model, SerializerMixin):
-    __tablename__ = 'appointments'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    doctor_id = db.Column(db.Integer, db.ForeignKey('doctors.id'), nullable=False)
-    patient_id = db.Column(db.Integer, db.ForeignKey('patients.id'), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    time = db.Column(db.Time, nullable=False)
-    
-    doctor = db.relationship('Doctor', back_populates='appointments')
-    patient = db.relationship('Patient', back_populates='appointments')
-
-class Patient(db.Model, SerializerMixin):
-    __tablename__ = 'patients'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    dob = db.Column(db.Date, nullable=False)
-    gender = db.Column(db.String(10), nullable=False)
-    address = db.Column(db.String(255), nullable=False)
-    
-    appointments = db.relationship('Appointment', back_populates='patient', cascade='all, delete-orphan')
-    
     

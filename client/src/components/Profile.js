a2bdea4864dc7d65d50b3a9332185169b6fb4cc9
ednakiruai <edna.kiruai@student.moringaschool.com>
2024-07-15 00:29:13 +0300

    Done

---
+++ client/src/components/Profile.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from './navbar';
+
+
+function Profile () {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    speciality: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  function handleSave () {
+    // Add doctor to backend 
+    fetch("http://localhost:3000/doctors", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Doctor added:", data);
+        // Clear form after saving
+        setUser({
+          name: "",
+          email: "",
+          speciality: "",
+          password: "",
+        });
+      });
+  };
+
+  function handleViewAppointments(){
+    navigate('/dms/patients');
+  };
+
+  return (
+      
+      <div className="profile-container">
+        <Navbar />
+        <h1>Welcome to Gotham Hospital</h1>
+        <form className="form-container">
+          <div className="input-container">
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={user.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Speciality:</label>
+            <input
+              type="text"
+              name="speciality"
+              value={user.speciality}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          
+          <button type="button" onClick={handleSave}>Save</button>
+        </form>
+        <button className="view-appointments-button" onClick={handleViewAppointments}>Enter</button>
+      
+
+        <div className="business-hours">
+          <h2>Business Hours</h2>
+          <p>Monday - Friday: 9:00 AM - 5:00 PM </p>
+          <p>Saturday: 9:00 AM - 3:00 PM </p>
+        </div>
+
+      
+
+        <div className="contact-info">
+          <h2>Contact Us</h2>
+          <p>Phone: +123 456 7890</p>
+          <p>Email: info@gothamhospital.com</p>
+          <p>Address: Business Arcade, Nairobi</p>
+        </div>
+      </div>
+    
+  );
+};
+
+export default Profile;

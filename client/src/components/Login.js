3f5a5e7af179260c21afae89b34d109e7e087d7b
jrubiaaaa <rubiaj3206@gmail.com>
2024-07-14 23:26:17 +0300

    formik

---
+++ client/src/components/Login.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+export const LoginForm = () => {
+  const [doctors, setDoctors] = useState([{}]);
+  const [refreshPage, setRefreshPage] = useState(false);
+  useEffect(() => {
+    fetch("/<doctor login route>")
+      .then((res) => res.json())
+      .then((data) => {
+        setDoctors(data);
+      });
+  }, [refreshPage]);
+  const formSchema = yup.object().shape({
+    email: yup.string().email("Incorrect email or password.Try again.").required("Please enter email."),
+    password: yup
+      .string()
+      .password("Incorrect email or password.Try again.")
+      .required("Please enter password."),
+  });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      fetch("/<doctor login route>", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values, null),
+      }).then((res) => {
+        if (res.status == 200) {
+          setRefreshPage(!refreshPage);
+        }
+      });
+    },
+  });
+  return (
+    <div>
+      <h1>Doctor Login Form</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <br />
+        <input
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <p style={{ color: "red" }}> {formik.errors.email}</p>
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          id="password"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <p style={{ color: "red" }}> {formik.errors.password}</p>
+        <button type="submit">Submit</button>
+      </form>
+      <table style={{ padding: "15px" }}>
+        <tbody>
+          <tr>
+            <th>Email</th>
+            <th>Password</th>
+          </tr>
+          {doctors === "undefined" ? (
+            <p>Loading</p>
+          ) : (
+            doctors.map((doctor, i) => (
+              <>
+                <tr key={i}>
+                  <td>{doctor.email}</td>
+                  <td>{doctor.password}</td>
+                </tr>
+              </>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
